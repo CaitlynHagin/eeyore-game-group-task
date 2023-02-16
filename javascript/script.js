ff54d1a23210d4731fc6746936589cd6aaea8163
CaitlynHagin <caitlyn32h@gmail.com>
2023-02-16 14:19:19 +0000

    second commit, moving target

--- javascript/script.js
+++ javascript/script.js
@@ -6,3 +6,14 @@ window.addEventListener("click", function (event){
 
     console.log(position)
 })
+
+   let interval = setInterval(function() {
+   let div = document.getElementById("donkeyTarget");
+   div.style.top = div.offsetTop + 4+ "px";
+   div.style.left = div.offsetLeft + 4+ "px";   },
+   500);
+
+   let bodyWidth = window.innerWidth;
+   let bodyHeight = window.innerHeight;
+   console.log(bodyWidth)
+   console.log(bodyHeight)

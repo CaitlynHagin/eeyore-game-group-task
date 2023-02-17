a7fd6879238aea243b39bc3f21d361fbcb1ea0ef
nth linguist Nathan <nth.linguist@gmail.com>
2023-02-17 22:42:38 +0000

    cursor event
    cursor event implement and styled - Nth

--- README.md
+++ README.md
@@ -1,9 +1,29 @@
 # eeyore-game-group-task
-group task of creating a game 
 
+group task of creating a game
 
 In groups - build a darts game, where the user clicks on a point and the dart goes to the particular point on the page.
 
 Break it down into stages,how you are going to try to build the game.
 
 Come up with a simple version of the functionality first and then make it more elaborate after.
+
+### Nathan
+
+> next ideas
+>
+> - onclick event where tail sticks on page
+>
+> - tail cursor disappears
+>
+> - click nailhole to launch win event
+>
+> inspiration
+>
+> - custom cursor codepen
+>
+> - https://codepen.io/esin87/pen/PoOKNqz
+>
+> - drag example
+>
+> - https://www.disneyclips.com/funstuff/donkey.html
--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,35 @@
-window.addEventListener("click", function (event){
-    let xPosition = event.clientX;
-    let yPosition = event.clientY;
+//Esin Saribudak custom cursor codepen
+// Cached DOM elements
+// Instructions Element
+const instructionsEl = document.querySelector(".instructions");
+// Custom Cursor Element
+const customCursorEl = document.getElementById("custom-cursor");
 
-    let position  = xPosition + yPosition
+// Event listener
+window.addEventListener("mousemove", (e) => {
+  //   Store x and y values of mousemove event
+  const x = e.clientX;
+  const y = e.clientY;
+  //   Update instructions text
+  instructionsEl.innerText = `Position: ${x}, ${y}`;
+  //   Set coordinates of custom cursor element
+  customCursorEl.style.top = `${y}px`;
+  customCursorEl.style.left = `${x}px`;
+});
 
-    console.log(position)
-})
+// // Esin Saribudak custom cursor codepen
+
+// @prgrx custom cursor codepen
+// let div = document.createElement("div");
+// div.style.width = "18px";
+// div.style.height = "18px";
+// div.style.borderRadius = "50%";
+// div.style.backgroundColor = "black";
+// div.style.position = "absolute"; // <--
+// document.body.appendChild(div);
+
+// window.addEventListener("mousemove", function (event) {
+//   div.style.left = event.pageX + "px";
+//   div.style.top = event.pageY + "px";
+// });
+// // @prgrx custom cursor codepen
